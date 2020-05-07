a0fbceee29f1614691d95c28b9899a1ae395dfe9
Bence Fábián <begnoc@gmail.com>
2020-05-07 14:51:50 +0200

    getting counter

--- static/js.js
+++ static/js.js
@@ -1,1 +1,14 @@
-console.log('hello from js')
+function initPage(counterPromise) {
+    counterPromise.then(result => console.log(result.value))
+}
+
+function dummyResponse() {
+    return Promise.resolve({value: 6});
+}
+
+function main() {
+    const counterPromise = fetch('/counter');
+    counterPromise.then(response => initPage(response.json));
+}
+
+main()
